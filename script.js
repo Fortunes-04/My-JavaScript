6fd37bb0fa5763de1e54a5e671036d2c60150daf
Tobi Ayeni <81029916+Fortunes-04@users.noreply.github.com>
2022-06-21 21:13:51 +0100

    first commit

---
+++ script.js
@@ -0,0 +1,3 @@
+// document.getElementById("task").innerHTML="My Ayeni Oluwatobi Fortunes, 6.5ft tall. Nigeria"
+
+document.write("<h2>NAME: Ayeni Oluwatobi Fortunes</h2><h2>HEIGHT: 6.5ft</h2><h2>COUNTRY: Nigeria</h2>")
